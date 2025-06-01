a10ff328796bfe63be7e9c9a17cc830787807197
vasanthtirumalasetti <vasanthtirumalasetti@gmail.com>
2025-06-01 23:32:15 +0530

    adding all

---
+++ script.js
@@ -0,0 +1,10 @@
+// script.js
+document.querySelectorAll('nav a').forEach(link => {
+  link.addEventListener('click', e => {
+    e.preventDefault();
+    const target = document.querySelector(link.getAttribute('href'));
+    if (target) {
+      target.scrollIntoView({ behavior: 'smooth' });
+    }
+  });
+});
